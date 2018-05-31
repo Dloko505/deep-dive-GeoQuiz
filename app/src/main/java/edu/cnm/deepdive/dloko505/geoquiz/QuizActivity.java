5e91665c1a81e51b075b861eb5773a33d68f830e
Dloko505 <Dloko505@gmail.com>
2018-05-30 21:36:26 -0600

    Added a previous button and changed text field to include next question feature. Also, changed next and prev to arrows instead of text.

--- app/src/main/java/edu/cnm/deepdive/dloko505/geoquiz/QuizActivity.java
+++ app/src/main/java/edu/cnm/deepdive/dloko505/geoquiz/QuizActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -11,7 +12,8 @@ public class QuizActivity extends AppCompatActivity {
 
   private Button mTrueButton;
   private Button mFalseButton;
-  private Button mNextButton;
+  private ImageButton mNextButton;
+  private ImageButton mPrevButton;
   private TextView mQuestionTextView;
 
   private Question[] mQuestionBank = new Question[]{
@@ -31,6 +33,13 @@ public class QuizActivity extends AppCompatActivity {
     setContentView(R.layout.activity_quiz);
 
     mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+    mQuestionTextView.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+        updateQuestion();
+      }
+    });
 
     mTrueButton = (Button) findViewById(R.id.true_button);
     mTrueButton.setOnClickListener(new View.OnClickListener() {
@@ -47,7 +56,17 @@ public class QuizActivity extends AppCompatActivity {
         checkAnswer(false);
       }
     });
-    mNextButton = (Button) findViewById(R.id.next_button);
+
+    mPrevButton = (ImageButton) findViewById(R.id.prev_button);
+    mPrevButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        mCurrentIndex = (mCurrentIndex - 1) % mQuestionBank.length;
+        updateQuestion();
+      }
+    });
+
+    mNextButton = (ImageButton) findViewById(R.id.next_button);
     mNextButton.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
@@ -55,7 +74,6 @@ public class QuizActivity extends AppCompatActivity {
         updateQuestion();
       }
     });
-
     updateQuestion();
   }
 
